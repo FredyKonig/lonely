9417b7ab2f65a6c1c5750f524dff8c06fddd8837
fkonig <fkonig@ordi.(none)>
2011-07-09 12:38:22 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lonely::Application.config.secret_token = '7f5a9e792e501c06f57ff0d4cf276f1a96dbb4b9ca36210e42484eabdea17bd0f0982360e0d40d2fb2b02de6f6ed148f0ba185b265b434a3a3726e810b222ff6'
