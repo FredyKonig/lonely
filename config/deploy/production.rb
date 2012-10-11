2a45a77186e3903a57c04b5d741194c598de889e
onja <andriamasinoroonja@yahoo.fr>
2012-10-11 15:00:38 +0300

    install thin

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ server "176.58.110.189", :app, :web, :db, :primary => true # list of the server
 
 set :default_environment, {
    'PATH' => "/home/master/.rvm/gems/ree-1.8.7-2012.02/bin:/home/master/.rvm/gems/ree-1.8.7-2012.02@global/bin:/home/master/.rvm/rubies/ree-1.8.7-2012.02/bin:/home/master/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
-   #'RUBY_VERSION' => 'ruby 1.8.7',
+   'RUBY_VERSION' => 'ruby 1.8.7',
    'GEM_HOME'     => "/home/master/.rvm/gems/ree-1.8.7-2012.02",
    'GEM_PATH'     => "/home/master/.rvm/gems/ree-1.8.7-2012.02:/home/master/.rvm/gems/ree-1.8.7-2012.02@global"
    #'BUNDLE_PATH'  => '/home/deploy/.rvm/gems/ruby-1.8.7-p302@leadnuke'  # If you are using bundler.
